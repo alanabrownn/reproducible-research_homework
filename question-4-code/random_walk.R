950f2175f3cefced5619a404cff2d16bec8b0dd3
Alana Brown <your.email@example.com>
2023-12-06 12:21:37 +0000

    modified code (reproducible)

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -9,6 +9,8 @@ library(gridExtra)
 
 random_walk  <- function (n_steps) { 
   
+  set.seed(23)
+  
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
 
   df[1,] <- c(0,0,1)
